6b0476f69edffdc57223b4c66e1297c581b862a4
mac4icken <128385483+mac4icken@users.noreply.github.com>
2023-09-03 04:11:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const mysql = require('mysql') 
-const PORT = 8080
+const PORT = 443
 let DB = false
 DB = mysql.createConnection({
     host: 'localhost',     
